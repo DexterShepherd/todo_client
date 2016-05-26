467f7c10330e2a744975f54b423a56743c3ae84c
Dexter Shepherd <dexter@kadenze.com>
2016-05-26 11:09:33 -0700

    refactored prints into methods, added print after operation to deal with changing index

--- todo_client.rb
+++ todo_client.rb
@@ -8,15 +8,41 @@ todo_path = ENV['TODO_PATH']
 if File.read(todo_path).empty?
   File.open(todo_path, 'w'){|f| f.write([{task: 'dummy', status: 'done'},{task: 'dummy', status: 'done'}].to_json)}
 end
-todos = JSON.parse(File.read(todo_path))
+@todos = JSON.parse(File.read(todo_path))
+
+def print_open
+  open = @todos.select do |i|
+    i['status'] == "open"
+  end
+  open.each_with_index do |val, index|
+    print "#{index} | "
+    print "#{val['status']}".colorize(:red)
+    print " | "
+    puts "#{val['task']}".colorize(:blue)
+    val["notes"].each do |i|
+      puts "            - #{i}"
+    end
+  end
+end
+
+def print_all
+  @todos.each_with_index do |val, index|
+    print "#{index} | "
+    val['status'] == 'done' ? color = :light_blue : color = :red
+    print "#{val['status']}".colorize(color)
+    print " | "
+    puts "#{val['task']}".colorize(:blue)
+  end
+end
 
 OptionParser.new do |parser|
   parser.on("-a", "--add TODO", "The todo to be added") do |a|
-    todos.unshift({task: a, status: "open", notes: []})
+    @todos.unshift({"task" => a, "status" => "open", "notes" => []})
+    print_open
   end
 
   parser.on("-i", "--index INDEX", Integer, "select todo[INDEX] to operate on") do |i| 
-    @selected = todos[i]
+    @selected = @todos[i]
   end
 
   parser.on("-c", "--complete [INDEX]", Integer, "complete selected" ) do |c|
@@ -24,42 +50,27 @@ OptionParser.new do |parser|
       puts "not nil"
       @selected['status'] = "done" 
     else
-      todos[c]['status'] = "done"
+      @todos[c]['status'] = "done"
     end
+    print_open
   end
 
   parser.on("-n", "--note NOTE", "add NOTE to seleced todo" ) do |n|
     @selected['notes'].push(n) unless @selected.nil?
+    print_open
   end
 
 
   parser.on("-l", "--list", "List open todos") do |l|
-    open = todos.select do |i|
-      i['status'] == "open"
-    end
-    open.each_with_index do |val, index|
-      print "#{index} | "
-      print "#{val['status']}".colorize(:red)
-      print " | "
-      puts "#{val['task']}".colorize(:blue)
-      val["notes"].each do |i|
-        puts "            - #{i}"
-      end
-    end
+    print_open
   end
 
   parser.on("--all", "list all todos") do |a|
-    todos.each_with_index do |val, index|
-      print "#{index} | "
-      val['status'] == 'done' ? color = :light_blue : color = :red
-      print "#{val['status']}".colorize(color)
-      print " | "
-      puts "#{val['task']}".colorize(:blue)
-    end
+    print_all
   end
 end.parse!
 
-todos = todos.sort do |a,b|
+@todos = @todos.sort do |a,b|
   if a["status"] == "open" && b["status"] == "done"
     -1
   elsif a["status"] == "done" && b["status"] == "open"
@@ -69,5 +80,5 @@ todos = todos.sort do |a,b|
   end
 end
 
-File.open(todo_path, 'w') {|f| f.write(todos.select{|i| i['task'] != 'dummy'}.to_json)}
+File.open(todo_path, 'w') {|f| f.write(@todos.select{|i| i['task'] != 'dummy'}.to_json)}
 
